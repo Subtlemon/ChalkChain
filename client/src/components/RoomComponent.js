5e3efcaa258e459b58d10dee50aff40f566e83cd
Subtlemon <harryliang1@gmail.com>
2019-04-20 23:32:53 -0400

    Added shared state in waiting room

--- client/src/components/RoomComponent.js
+++ client/src/components/RoomComponent.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 
+import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 
 const styles = {
@@ -19,7 +21,10 @@ export default class RoomComponent extends Component {
     this.roomName = props.roomName;
     this.uid = props.viewProps.uid;
     this.state = {
-      users: []
+      users: [],
+      sharedState: {
+        drawTime: 60,
+      },
     };
 
   }
@@ -29,7 +34,7 @@ export default class RoomComponent extends Component {
    ***************************************************************************/
 
   componentDidMount = () => {
-    // Register a listener for users to update state.
+    // Register a listener for users to update list of users in room.
     this.userRef = this.roomRef.child('users');
     this.userRef.on('value', (snapshot) => {
       // Construct array of object's values' nickName.
@@ -39,17 +44,41 @@ export default class RoomComponent extends Component {
         })
       });
     });
+
+    // Register a listener for shared room state, such as round time limit.
+    this.sharedRef = this.roomRef.child('waiting_room_state');
+    this.sharedRef.on('value', (snapshot) => {
+      if (snapshot.val()) {
+        this.setState({
+          sharedState: snapshot.val()
+        });
+      }
+    });
   }
 
   componentWillUnmount() {
     this.userRef.off('value');
+    this.sharedRef.off('value');
+  }
+
+  /***************************************************************************
+   * Button Events                                                           *
+   ***************************************************************************/
+
+  handleSaveButton = (event) => {
+    // This assumes set cannot fail.
+    this.sharedRef.set(this.state.sharedState);
+  }
+
+  handleStartButton = (event) => {
+    console.log('Start game button pressed.');
   }
 
   /***************************************************************************
    * Render                                                                  *
    ***************************************************************************/
 
-  getList = () => {
+  getUserListItems = () => {
     return this.state.users.map((user) => {
       return (
         <ListItem>
@@ -59,14 +88,32 @@ export default class RoomComponent extends Component {
     });
   }
 
+  // TODO: This is ugly AF.
   render() {
     return (
       <div style={styles.layout}>
         <Typography>
           Room name: {this.roomName}
         </Typography>
+        <TextField
+          label='Seconds per drawing'
+          value={this.state.sharedState.drawTime}
+          onChange={(event) => this.setState({sharedState: {drawTime: event.target.value}})}
+        />
+        <Button
+          variant='contained'
+          onClick={this.handleSaveButton}
+        >
+          Save Settings
+        </Button>
+        <Button
+          variant='contained'
+          onClick={this.handleStartButton}
+        >
+          Start Game
+        </Button>
         <List>
-          {this.getList()}
+          {this.getUserListItems()}
         </List>
       </div>
     );
