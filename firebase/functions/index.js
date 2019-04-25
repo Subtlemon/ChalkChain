74b174fdabc2c29c73e829e742ac0c4e27d1bff1
Subtlemon <harryliang1@gmail.com>
2019-04-25 12:12:45 -0400

    Removed old cloud function

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -11,25 +11,10 @@ const functions = require('firebase-functions');
 exports.cleanRoom = functions.database.ref('/rooms/{roomName}/users')
     .onDelete((snapshot, context) => {
       const roomName = context.params.roomName;
-      console.log('Room ' + roomName + ' has no users. Deleting...');
       return snapshot.ref.parent.remove();
     });
 
-// TODO: Delete this once flask is no longer deployed.
-// When all users have left a game, delete all associated data.
-exports.finishGame = functions.database.ref('/rooms/{roomName}/in_game')
-    .onDelete((snapshot, context) => {
-      const roomName = context.params.roomName;
-      console.log('Room ' + roomName + ' has finished their game.',
-        'Removing game information...');
-      return Promise.all([
-        snapshot.ref.parent.child('chains').remove(),
-        snapshot.ref.parent.child('game_state').remove(),
-        snapshot.ref.parent.child('spectate_state').remove(),
-      ]);
-    });
-
-// When all users have left a game, delete all associated data.
+// When all users have left a game, delete associated game data.
 exports.cleanGame = functions.database.ref('/rooms/{roomName}/game/activePlayers')
     .onDelete((snapshot, context) => {
       return snapshot.ref.parent.remove();
