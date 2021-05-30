4fd6f3c60332657477fafcc48290e222c5c9aaa7
Subtlemon <harryliang1@gmail.com>
2021-05-30 00:58:46 -0400

    Update firebase functions to use nodejs14

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,21 +1,22 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 // exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
 // });
 
 // When all users have left a room, delete it.
-exports.cleanRoom = functions.database.ref('/rooms/{roomName}/users')
+exports.cleanRoom = functions.database.ref("/rooms/{roomName}/users")
     .onDelete((snapshot, context) => {
-      const roomName = context.params.roomName;
       return snapshot.ref.parent.remove();
     });
 
 // When all users have left a game, delete associated game data.
-exports.cleanGame = functions.database.ref('/rooms/{roomName}/game/activePlayers')
-    .onDelete((snapshot, context) => {
-      return snapshot.ref.parent.remove();
-    });
+exports.cleanGame =
+    functions.database.ref("/rooms/{roomName}/game/activePlayers")
+        .onDelete((snapshot, context) => {
+          return snapshot.ref.parent.remove();
+        });
